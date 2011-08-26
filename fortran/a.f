937178686a630ec50c27bb508a1799a0c9e27898
NAKAMURA Takahide <nkmrtkhd@gmail.com>
2011-08-26 11:03:53 +0900

    modify sample data

--- fortran/a.f
+++ fortran/a.f
@@ -1,5 +1,5 @@
       program make
-c     Last modified <2011-08-23 14:22:09 by NAKAMURA Takahide>
+c     Last modified <2011-08-26 10:23:18 by NAKAMURA Takahide>
 
       implicit none
       integer i,n
@@ -10,8 +10,8 @@ c     Last modified <2011-08-23 14:22:09 by NAKAMURA Takahide>
       do i=1,n
         write(11,'(20e12.4)')cos((i-1)*3.1415d0/dble(n)*2),
      &       sin((i-1)*3.1415d0/dble(n)*2)
-        write(22,'(20e12.4)')cos((i-1)*3.1415d0/dble(n)*2)/3d0,
-     &       sin((i-1)*3.1415d0/dble(n)*2)/2d0
+        write(22,'(20e12.4)')cos((i-1)*3.1415d0/dble(n)*2)*2d0,
+     &       sin((i-1)*3.1415d0/dble(n)*2)*3d0
       enddo
 
 
